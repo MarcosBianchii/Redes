929127ab3154deb72897dbdf23e4fc515d5aafd8
MarcosBianchii <marcosbianchi07@gmail.com>
2024-09-15 11:39:27 -0300

    three_way_handshake

--- TP1/src/rdp_socket/socket.py
+++ TP1/src/rdp_socket/socket.py
@@ -24,21 +24,26 @@ class RdpSocket:
         Establishes a new connection to a RdpListener
         """
         self = cls((ip, port))
+        syn_pkt = Packet.syn_pkt()
+        syn_pkt_bytes = syn_pkt.encode()
+        self._skt.settimeout(SYN_TIMEOUT)
 
         while True:
-            syn_pkt = Packet.syn_pkt()
-            _sendall(self._skt, syn_pkt.encode(), self.peer_addr())
+            _sendall(self._skt, syn_pkt_bytes, self.peer_addr())
 
             try:
-                self._skt.settimeout(SYN_TIMEOUT)
-                header, peer_addr = self._skt.recvfrom(PKT_HEADER_SIZE)
+                header, (p_ip, p_port) = self._skt.recvfrom(PKT_HEADER_SIZE)
             except timeout:
                 continue
 
-            pkt = Packet.from_header(header)
-            if pkt.is_syn() and pkt.is_ack():
-                self._peer_addr = peer_addr
-                break
+            # Check this is a new connection from the same host
+            if p_ip == ip and p_port != port:
+                pkt = Packet.from_header(header)
+                if pkt.is_syn() and pkt.is_ack():
+                    self._peer_addr = (p_ip, p_port)
+                    syn_ack_pkt = Packet.syn_ack_pkt()
+                    _sendall(self._skt, syn_ack_pkt.encode(), self.peer_addr())
+                    break
 
         return self
 
@@ -55,13 +60,14 @@ class RdpSocket:
         """
         Sends the bytes in `data` through the socket
         """
+        self.settimeout(ACK_TIMEOUT)
+
         for pkt in Packet.make_pkts(data):
             pkt_bytes = pkt.encode()
             while True:
                 _sendall(self._skt, pkt_bytes, self.peer_addr())
 
                 try:
-                    self.settimeout(ACK_TIMEOUT)
                     header = self._skt.recv(PKT_HEADER_SIZE)
                 except timeout:
                     continue
@@ -90,7 +96,22 @@ class RdpListener:
     def _create_stream(cls, peer_addr) -> RdpSocket:
         stream = RdpSocket(peer_addr)
         syn_ack = Packet.syn_ack_pkt()
-        _sendall(stream._skt, syn_ack.encode(), peer_addr)
+        syn_ack_bytes = syn_ack.encode()
+        stream.settimeout(SYN_TIMEOUT)
+
+        while True:
+            _sendall(stream._skt, syn_ack_bytes, stream.peer_addr())
+
+            try:
+                header, addr = stream._skt.recvfrom(PKT_HEADER_SIZE)
+            except timeout:
+                continue
+
+            if addr == peer_addr:
+                pkt = Packet.from_header(header)
+                if pkt.is_syn() and pkt.is_ack():
+                    break
+
         return stream
 
     def close(self):
