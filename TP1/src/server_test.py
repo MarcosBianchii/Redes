a99a4e6cb8d930eae4b28d4ea12dcb15fc4ccc03
MarcosBianchii <marcosbianchi07@gmail.com>
2024-09-15 21:12:41 -0300

    cerrado del listener handler con signals

--- TP1/src/server_test.py
+++ TP1/src/server_test.py
@@ -1,3 +1,4 @@
+import signal
 from sys import argv
 from rdp_socket.socket import RdpListener
 
@@ -9,6 +10,17 @@ puerto = int(argv[1])
 listener = RdpListener.bind(puerto)
 print(f"Escuchando en el puerto: {puerto}")
 
+
+def close_listener(sig, frame):
+    listener.close()
+    exit(0)
+
+
+signal.signal(signal.SIGINT, close_listener)
+
+
 for stream in listener:
     print(f"Me llego una coneccion de: {stream.peer_addr()}")
     stream.close()
+
+listener.close()
