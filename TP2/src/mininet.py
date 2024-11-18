0196a0501717ec2d7197178916257016c0e47334
MarcosBianchii <marcosbianchi07@gmail.com>
2024-11-18 15:40:52 -0300

    preguntas-teoricas

--- TP2/src/mininet.py
+++ TP2/src/mininet.py
@@ -1,6 +1,13 @@
 from mininet.topo import Topo
 
 
+"""
+H1 -[1]+                                              +[2]- H3
+       +- S1 -[2]-[1]- S2 -[2]-[1]- ... -[2]-[1]- SN -+
+H2 -[3]+                                              +[3]- H4
+"""
+
+
 class Chain(Topo):
     def __init__(self, n: int, **opts):
         super(self.__class__, self).__init__(self, opts)
@@ -14,10 +21,11 @@ class Chain(Topo):
         h4 = self.addHost("h4")
 
         switches = [self.addSwitch(f"s{i + 1}") for i in range(n)]
+        self.addLink(h1, switches[0])
+
         for i in range(1, n):
             self.addLink(switches[i - 1], switches[i])
 
-        self.addLink(h1, switches[0])
         self.addLink(h2, switches[0])
         self.addLink(h3, switches[-1])
         self.addLink(h4, switches[-1])
